2833135c713cdfd2adf7352c4cf801817a76f8b0
Pandula Mandawala <pandula95@gmail.com>
2019-08-17 22:34:38 +0530

    fixed some css

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -4,10 +4,10 @@ import classes from './BuildControls.module.css';
 
 
 const control = [
-    {label: 'සලාද ', type:'salad'},
-    {label: 'හරක් මස්', type:'bacon'},
-    {label: 'චීස් ', type:'cheese'},
-    {label: 'කුකුල් මස්', type:'meat'}
+    {label: 'salad ', type:'salad'},
+    {label: 'bacon', type:'bacon'},
+    {label: 'cheese', type:'cheese'},
+    {label: 'meat', type:'meat'}
 ];
 
 const buildControls = (props) => (
